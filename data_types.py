f4e090dd8075f8fe5bbe7a53e1110fe26bc2bf17
dar1an4 <Lepa1994>
2022-07-26 22:14:04 +0300

    homework practice 1

---
+++ data_types.py
@@ -0,0 +1,22 @@
+age = 27
+name, surname = 'Alex', 'Pro'
+leaving_city = 'Odessa'
+dog_name = 'Morty'
+height = 185
+weight = 70
+profession = 'None'
+speaking_language_native, speaking_language_second, speaking_language_third = 'Ukranian', 'English', 'Russian'
+car = 'Meanult Reagan'
+print(age, type(age))
+print(name, type(name))
+print(surname, type(surname))
+print(leaving_city, type(leaving_city))
+print(dog_name, type(dog_name))
+print(height, type(height))
+print(weight, type(weight))
+print(profession, type(profession))
+print(speaking_language_native, type(speaking_language_native))
+print(speaking_language_second, type(speaking_language_second))
+print(speaking_language_third, type(speaking_language_third))
+print(car, type(car))
+
