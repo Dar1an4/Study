98f5a0bc2b8fc231f2a8dba8648077e66ae4b941
dar1an4 <Lepa1994>
2022-07-28 11:39:57 +0300

    homework practice 2 fix again...

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -3,5 +3,5 @@ number = float(input(f'Hello dear {name.title()}, enter any float value, please:
 print(f'Your number is {number}',
       f'\nAnd integer value of yor number is {int(number)}',
       f'\nThe fourth power of yor number is {int(number)**4}',
-      f'\nThe second root of your number is {int(number)*0.5}',
+      f'\nThe second root of your number is {int(number)**0.5}',
       f'\nRemainder of division by two of your number is {int(number)%2}')
