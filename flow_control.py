ba6af7416971f270ee308e7d2104d54f33fcba6b
Dar1an4 <ilepa.od@gmail.com>
2022-08-03 22:33:38 +0300

    First Try of pythonculator O_o fix upd

--- flow_control.py
+++ flow_control.py
@@ -5,83 +5,65 @@ print(f'Hello, dear {name}\n'
       f" Let's try to show you Pythoculator \n"
       f' You just need enter any Value one by one below, and finally, '
       f' what whe need to do ? ("+","-","/","*", "n ** n1")')
-values = []         #  use it later, put here our values for sorting for comparison
-counter = 0
-while counter == 0:
+
+while 1:
     value_input_first = input('Enter first value: ')
     value_input_second = input('Enter second value: ')
     try:
-        if '.' in value_input_first or '.' in value_input_second:
+        if '.' in value_input_first:
             value_first = float(value_input_first)
-            value_second = float(value_input_second)
-            counter += 1
         else:
             value_first = int(value_input_first)
+        if '.' in value_input_second:
+            value_second = float(value_input_second)
+            break
+        else:
             value_second = int(value_input_second)
-            counter += 1
+            break
     except:
         print('Hey Bro, wtf, what has been entered? we need only digits')
 
-value_input_first_type = 'float' if '.' in value_input_first else 'int'
-value_input_second_type = 'float' if '.' in value_input_second else 'int'
 
-values.append(value_input_first)
-values.append(value_input_second)
+# value_input_first_type = 'float' if '.' in value_input_first else 'int'
+# value_input_second_type = 'float' if '.' in value_input_second else 'int'
+
+values = [value_input_first, value_input_second]
 values.sort()       #  sort for comparison
                                                                     # use it in answer for comparison between values
 comparison = '<' if float(values[0]) < float(values[1]) else '='    # use float because if type is different - comparison will be
                                                                     # incorrect, when values ==
-counter = 0     #  refresh our iteration counter
-while counter == 0:
-    to_do = input('finally, what whe need to do with them: ')
-    if to_do == '+':
-          result = value_first + value_second
-          print(f'\n \nand the answer is..... : {result} (how unexpected) \n'
-                f'And just for reference: \n'
-                f'Type of your answer is {type(result)} \n'
-                f'And {values[0]} {comparison} {values[1]} \n'
-                f'Type of your first value is {value_input_first_type}, and type of second is {value_input_second_type}\n'
-                f'the order of answer is {len(str(int(result)))-1}')      #How many orders does the operand have? (скільки порядків)
-          counter += 1
-    elif to_do == '-':
-          result = value_first - value_second
-          print(f'\n \nand the answer is..... : {result} (how unexpected) \n'
-                f'And just for reference: \n'
-                f'Type of your answer is {type(result)} \n'
-                f'And {values[0]} {comparison} {values[1]} \n'
-                f'Type of your first value is {value_input_first_type}, and type of second is {value_input_second_type}\n'
-                f'the order of answer is {len(str(int(result)))-1}')      #How many orders does the operand have? (скільки порядків)
-          counter += 1
-    elif to_do == '/':
-          result = value_first / value_second
-          print(f'\n \nand the answer is..... : {result} (how unexpected) \n'
-                f'And just for reference: \n'
-                f'Type of your answer is {type(result)} \n'
-                f'And {values[0]} {comparison} {values[1]} \n'
-                f'Type of your first value is {value_input_first_type}, and type of second is {value_input_second_type}\n'
-                f'the order of answer is {len(str(int(result)))-1}')      #How many orders does the operand have? (скільки порядків)
-          counter += 1
-    elif to_do == '*':
-          result = value_first * value_second
-          print(f'\n \nand the answer is..... : {result} (how unexpected) \n'
-                f'And just for reference: \n'
-                f'Type of your answer is {type(result)} \n'
-                f'And {values[0]} {comparison} {values[1]} \n'
-                f'Type of your first value is {value_input_first_type}, and type of second is {value_input_second_type}\n'
-                f'the order of answer is {len(str(int(result)))-1}')      #How many orders does the operand have? (скільки порядків)
-          counter += 1
-    elif to_do == '**':
-          result = value_first ** value_second
-          print(f'\n \nand the answer is..... : {result} (how unexpected) \n'
-                f'And just for reference: \n'
-                f'Type of your answer is {type(result)} \n'
-                f'And {values[0]} {comparison} {values[1]} \n'
-                f'Type of your first value is {value_input_first_type}, and type of second is {value_input_second_type}\n'
-                f'the order of answer is {len(str(int(result)))-1}')      #How many orders does the operand have? (скільки порядків)
-          counter += 1
-    else:
-        print(f'{name}, bro, wtf again? We already had a deal! you need use only +, -, /, *, ** "\n'
-              f'Dont forget! your values is {value_first} and {value_second}!\n'
-              f'Just put between them something what we talking about!')
+while 1:
+    try:
+        to_do = input('finally, what whe need to do with them: ')
+        if to_do == '+':
+              result = value_first + value_second
+              break
+        elif to_do == '-':
+              result = value_first - value_second
+              break
+        elif to_do == '/':
+              result = value_first / value_second
+              result = float(result) if '.' in value_input_first or '.' in value_input_second else int(result)
+              break
+        elif to_do == '*':
+              result = value_first * value_second
+              break
+        elif to_do == '**':
+              result = value_first ** value_second
+              break
+        else:
+            print(f'{name}, bro, wtf again? We already had a deal! you need use only +, -, /, *, ** "\n'
+                  f'Dont forget! your values is {value_first} and {value_second}!\n'
+                  f'Just put between them something what we talking about!')
+    except Exception as error:
+        print(f"You make dirty... :{ error }")
+
+print(f'\n \nand the answer is..... : {result} (how unexpected) \n'
+      f'And just for reference: \n'
+      f'Type of your answer is {type(result)} \n'
+      f'And {values[0]} {comparison} {values[1]} \n'
+      f'Type of your entered first value is {type(value_first)}, and type of second is {type(value_second)}\n'
+      f'the order of answer is {len(str(int(result))) - 1}')  # How many orders does the operand have? (скільки порядків)
+
 
 print(f'\n\n Thx for using that! waiting for you again!')
