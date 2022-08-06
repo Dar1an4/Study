cccd2b864f5cfda0dd99476e8541ed126970626d
dar1an4 <Lepa1994>
2022-08-06 10:20:05 +0300

    pythonculator O_o upd (last fix)

--- flow_control.py
+++ flow_control.py
@@ -5,6 +5,7 @@ to_dos = ['+', '-', '/', '*', '**']
 
 text_counter = 0
 counter = 1 if name == 'Exit' else 0
+
 while counter == 0:
     if text_counter == 0:
         print(f' Hello, dear {name}\n'
@@ -13,9 +14,9 @@ while counter == 0:
               f' what whe need to do ? {to_dos}\n'
               f' and if you want to exit - just input "Exit" instead any value or operation')
     else:
-        print("Let's go again.\n"
-              'if you want to exit - just input "Exit".\n'
-              'For continue, just input some values and what we need to do: \n')
+        print(" Let's go again.\n"
+              ' if you want to exit - just input "Exit".\n'
+              f' For continue, just input any two values and what we need to do {to_dos}: \n')
     while counter == 0:
         value_input_first = input('Enter first value: ')
         if value_input_first.title().strip() == 'Exit':
@@ -56,22 +57,23 @@ while counter == 0:
                 break
             elif to_do not in to_dos:
                 if to_do.title().strip() == 'Exit':
+                    counter += 1
                     break
                 else:
-                    print(f'{name}, bro, wtf again? We already had a deal! you need use only {to_dos}'
+                    print(f'{name}, bro, wtf again? We already had a deal! you need use only {to_dos}\n'
                           f'Dont forget! your values is {value_first} and {value_second}!\n'
                           f'Just put between them something what we talking about!')
         except Exception as error:
             print(f"You make dirty... :{ error }")
-    try:
-        print(f'\nYour an arithmetical example is {value_first} {to_do} {value_second}\n'
-              f'and the answer is..... : {result} (how unexpected) \n'
-              f'And just for reference: \n'
-              f'Type of your answer is {type(result)} \n'
-              f'And {values[0]} {comparison} {values[1]} \n'
-              f'Type of your entered first value is {type(value_first)}, and type of second is {type(value_second)}\n'
-              f'the order of answer is {len(str(int(result))) - 1} \n')  # How many orders does the operand have?(скільки порядків)
+    if counter == 0:
+        print(f'\n Your an arithmetical example is {value_first} {to_do} {value_second}\n'
+              f' and the answer is..... : {result} (how unexpected) \n'
+              f' And just for reference: \n'
+              f' Type of your answer is {type(result)} \n'
+              f' And {values[0]} {comparison} {values[1]} \n'
+              f' Type of your entered first value is {type(value_first)}, and type of second is {type(value_second)}\n'
+              f' the order of answer is {len(str(int(result))) - 1} \n')  # How many orders does the operand have?(скільки порядків)
         text_counter += 1
-    except:
+    else:
         print('Ok, if u decide it.... So, by, see you later!')
         break
