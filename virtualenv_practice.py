ca30869cd100296786f907a3d663b41028ca3ecd
Dar1an4 <ilepa.od@gmail.com>
2022-09-03 13:32:21 +0300

    🚀 venv

---
+++ virtualenv_practice.py
@@ -0,0 +1,15 @@
+"""
+Step by step virtual commands
+
+1. mkdir py_venv
+2. cd py_venv
+3. python -m venv CV_venv
+4. cd CV_venv
+5. cd Scripts
+6. activate.bat
+7.  pip install django numpy pandas requests SQLAlchemy flask
+9. pip freeze
+10. pip freeze > requirements.txt
+11. deactivate
+
+"""
