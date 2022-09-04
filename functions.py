77d65a76559e6bb4d84d68abaa4088fc684399a8
Dar1an4 <ilepa.od@gmail.com>
2022-09-04 23:49:33 +0300

    🐇 some func fix

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 import time
 
+
 """FIRST EXERCISE"""
 start = int(input('enter the start of calculation: '))
 finish = int(input('enter the start of calculation: '))
+
 
 def counter(start: int, finish: int) -> int:
     list = [start, finish]
@@ -10,10 +12,12 @@ def counter(start: int, finish: int) -> int:
     result = sum(range(list[0], list[1] + 1))
     return result
 
+
 print(f'{counter(start, finish) = }')
 
 
 """SECOND EXERCISE"""
+
 
 def time_convert(sec: int) -> str:
     if sec < 86400:
@@ -28,6 +32,7 @@ def time_convert(sec: int) -> str:
         result = f'{str(days)}:{str(time.strftime("%H:%M:%S", res))}'
         return f'days:hours:minutes:seconds {result}'
 
+
 print(f'{time_convert(86401) = }')
 
 
@@ -35,15 +40,18 @@ print(f'{time_convert(86401) = }')
 
 list_inc = [1, 3, 4, 6, 7, 8, 12, 2]
 
+
 def for_counter(incoming_list: list) -> int or float:
     sum = 0
     for i in incoming_list:
         sum += i
     return sum
 
+
 print(f'{for_counter(list_inc) = }')
 
 list_inc = [1, 3, 4, 6, 7, 8, 12, 2]
+
 
 def while_counter(incoming_list: list) -> int or float:
     output_list = incoming_list.copy()
@@ -52,9 +60,11 @@ def while_counter(incoming_list: list) -> int or float:
         sum += output_list.pop()
     return sum
 
+
 print(f'{while_counter(list_inc) = }')
 
 list_inc = [1, 3, 4, 6, 7, 8, 12, 2]
+
 
 def recurs_counter(incoming_list: list) -> int or float:
     if incoming_list == []:
@@ -64,31 +74,39 @@ def recurs_counter(incoming_list: list) -> int or float:
         sum = sum + incoming_list[0]
         return sum
 
+
 print(f'{recurs_counter(list_inc) = }')
 
 
 """FOURTH EXERCISE"""
+
 
 def recurs_fibo(n: int) -> int:
     if n in (1, 2):
         return 1
     return recurs_fibo(n - 1) + recurs_fibo(n - 2)
 
+
 print(recurs_fibo(10))
 
 
 """FIFTH EXERCISE"""
 
+
 def ingr_burger(decor_this) -> any:
     def ingr_tomato():
         print('tomato')
+
     def ingr_meat():
         print('meat')
+
     def ingr_chees():
         print('chees')
+
     def ingr_bread():
         print('bread')
-    return ingr_tomato(), ingr_meat(), ingr_chees(), ingr_bread()
+    return ingr_tomato(), ingr_meat(), ingr_chees(), ingr_bread(), decor_this
+
 
 @ingr_burger
 def burger() -> None:
