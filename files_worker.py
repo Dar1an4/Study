eae160fd3fcaf894740189ac08eb6c9402b45fe6
Dar1an4 <ilepa.od@gmail.com>
2022-09-07 22:08:50 +0300

    🚀 csv json comtrol

--- files_worker.py
+++ files_worker.py
@@ -11,17 +11,16 @@ except FileExistsError:
     print('Already have the directory, Working...')
 
 
-file_path = "schedules/schedule.csv"
 fieldnames = ['Nomber', 'Working Company', 'Passanger Name', 'departure point', 'departure time', 'destination point',
               'arrival time', 'cost ticket']
 
-with open(file_path, "w", newline='') as fh:
+with open("schedules/schedule.csv", "w", newline='') as fh:
     writer = csv.DictWriter(fh, lineterminator='\n', fieldnames=fieldnames)
     writer.writeheader()
 
 address_ua, address_en, generic, finance, person = Address('uk'), Address('en'), Generic('ru'), Finance('en'), Person('uk')
 
-for i in range(200):
+for i in range(300):
     work_company = finance.company()
     pass_name = translit(person.full_name(), language_code='uk', reversed=True)
     dep_point = translit(address_ua.city(), language_code='uk', reversed=True)
@@ -29,24 +28,30 @@ for i in range(200):
     dest_point = address_en.city()
     arr_time = str(generic.datetime.time())[0:5]
     cost_ticket = finance.price(minimum=100, maximum=9000)
-    with open(file_path, "a", newline='') as fh:
+    with open("schedules/schedule.csv", "a", newline='') as fh:
         writer = csv.DictWriter(fh, lineterminator='\n', fieldnames=fieldnames)
         writer.writerow({'Nomber': i+1, 'Working Company': work_company, 'Passanger Name': pass_name,
                          'departure point': dep_point, 'departure time': dep_time, 'destination point': dest_point,
                          'arrival time': arr_time, 'cost ticket': cost_ticket})
 
-data_flew = []
-with open(file_path, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        data_flew.append(row)
-print(data_flew)
 
-
-with open(file_path, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+def reader_csvdb() -> dict:
     dict_flew = {}
-    for row in reader:
-        dict_flew.update({row.get('Nomber'): row})
-    with open("schedules/schedule.json", "a") as fh:
-        json.dump(dict_flew, fh, indent=4, sort_keys=True)
+    with open("schedules/schedule.csv", newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            dict_flew.update({row.get('Nomber'): row})
+    return dict_flew
+
+
+flew_db = reader_csvdb()
+print(flew_db)
+
+
+def json_writer(flew_db: dict, directory: str) -> None:
+    with open(directory, "w") as fh:
+        json.dump(flew_db, fh, indent=4, sort_keys=True)
+    print('succes dumped!')
+
+
+json_writer(flew_db, "schedules/schedule.json")
